cc3e43b0379179d440a70884df60f6899a15812b
Carl Tidelius <carl_tidelius@live.se>
2017-03-30 15:45:15 +0200

    Added the accelerometer, and tried to make the compass, but it failed.

--- app/src/main/java/com/example/carltidelius/myapplication/MainActivity.java
+++ app/src/main/java/com/example/carltidelius/myapplication/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.carltidelius.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Do Things button */
+    public void accelerometer(View view) {
+        Intent intent = new Intent(this, AccelerometerActivity.class);
+        startActivity(intent);
+    }
+    /** Called when the use taps the Do Compass Things button*/
+    public void compass(View view){
+        Intent intent = new Intent(this, CompassActivity.class);
+        startActivity(intent);
+
     }
 }
